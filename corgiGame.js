f30db0224cd223bf99c63b53a4e6ba364209b91c
John Kuhn <johnkuhn@dhcp-218-83.resnet.purdue.edu>
2016-10-23 01:50:46 -0400

    updated some images

--- corgiGame.js
+++ corgiGame.js
@@ -1,44 +1,44 @@
 jQuery.get('https://raw.githubusercontent.com/johnlk/corgiGame/master/images.txt', function(data){
-  var imageLinks = data.split('\n');
+    
+    var imageLinks = data.split('\n');
   
-  var maxWidth = Math.floor($(window).width()/160) * 33;
+    var maxWidth = Math.floor($(window).width()/160) * 33;
       
-  var picArea = document.getElementById('picArea');
-  var randomNum = Math.floor( (Math.random()*imageLinks.length) + 1 );
+    var picArea = document.getElementById('picArea');
+    var randomNum = Math.floor( (Math.random()*imageLinks.length) + 1 );
       
-  var link;
+    var link;
       
-  for (var i = 0; i < imageLinks.length; i++) {
-    link = imageLinks[i];
-    picArea.innerHTML += "<img class=\"pics\" src=" + link + " style=\"max-width: " + maxWidth + "px;\">";
-  }
+    for (var i = 0; i < imageLinks.length; i++) {
+        link = imageLinks[i];
+        picArea.innerHTML += "<img class=\"pics\" src=" + link + " style=\"max-width: " + maxWidth + "px;\">";
+    }
       
-  var index;
+    var index;
   
-  $('#picArea img').click(function (e){
+    $('#picArea img').click(function (e){
           
     index = $(this).index() + 1;
       
     if (index == randomNum) {
       
-      $('.pics').css("visibility", "hidden");
-      $(this).css("visibility", "visible");
+        $('.pics').css("visibility", "hidden");
+        $(this).css("visibility", "visible");
               
-      alert("You won!!!");
+        alert("You won!!!");
               
     }else{
               
-      if (index > randomNum) {
-        alert("You're below it");
-      }else{
-        alert("You're above it");
-      }
+        if (index > randomNum) {
+            alert("You're below it");
+        }else{
+            alert("You're above it");
+        }
               
-      $(this).css("visibility", "hidden");
+        $(this).css("visibility", "hidden");
               
     }
           
   });
-  
-  
+    
 });
